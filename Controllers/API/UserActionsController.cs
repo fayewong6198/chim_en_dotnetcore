862faab329d449fad40ac7eecbab04bd5b291c54
faye wong <trandaosimanh@gmail.com>
2020-09-15 14:24:29 +0700

    cc

--- Controllers/API/UserActionsController.cs
+++ Controllers/API/UserActionsController.cs
@@ -23,10 +23,16 @@ namespace Chim_En_DOTNET.Controllers.API
 
     }
 
-    [HttpPost("AddToCart")]
+    [HttpPost("Cart")]
     public async Task<IActionResult> AddToCart(AddToCartParams addToCartParams)
     {
-      Console.WriteLine(User.Identity.IsAuthenticated);
+      var product = await _context.Products.FindAsync(addToCartParams.ProductId);
+
+      if (product == null)
+      {
+        return NotFound(new { msg = "Product not found" });
+      }
+
 
       string sessionId = HttpContext.Request.Headers["SessionId"].FirstOrDefault();
 
@@ -68,6 +74,10 @@ namespace Chim_En_DOTNET.Controllers.API
 
       if (cartItem == null)
       {
+        if (product.Available < addToCartParams.Quantity)
+        {
+          return BadRequest(new { msg = $"Product only contains {product.Available} items less" });
+        }
         cartItem = new CartItem();
         cartItem.CartId = cart.Id;
         cartItem.ProductId = addToCartParams.ProductId;
@@ -77,6 +87,10 @@ namespace Chim_En_DOTNET.Controllers.API
 
       else
       {
+        if (product.Available < addToCartParams.Quantity + cartItem.Quantity)
+        {
+          return BadRequest(new { msg = $"Product only contains {product.Available} items less" });
+        }
         cartItem.Quantity = cartItem.Quantity + addToCartParams.Quantity;
       }
       await _context.SaveChangesAsync();
@@ -97,36 +111,168 @@ namespace Chim_En_DOTNET.Controllers.API
       return Ok(cart);
     }
 
-    [HttpPost("ChangeQuantity")]
-    public async Task<IActionResult> ChangeQuantity()
+    [HttpPut("Cart")]
+    public async Task<IActionResult> ChangeQuantity(ChangeQuantityParams changeQuantityParams)
     {
+      var product = await _context.Products.FindAsync(changeQuantityParams.ProductId);
+
+      if (product == null)
+      {
+        return NotFound(new { msg = "Product not found" });
+      }
 
       string sessionId = HttpContext.Request.Headers["SessionId"].FirstOrDefault();
+
+      if (string.IsNullOrEmpty(sessionId) && !User.Identity.IsAuthenticated)
+      {
+        return Unauthorized();
+      }
+
+      Cart cart = new Cart();
+      CartItem cartItem = new CartItem();
       if (User.Identity.IsAuthenticated)
       {
-        return Ok();
+        string userId = User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+        cart = await _context.Carts.Where(c => c.ApplicationUserId.Equals(userId)).FirstOrDefaultAsync();
 
       }
       else if (!string.IsNullOrEmpty(sessionId))
       {
-        return Ok();
+        cart = await _context.Carts.Where(c => c.SessionId.Equals(sessionId)).Include(c => c.CartItems).FirstOrDefaultAsync();
       }
-      return Unauthorized();
+
+      if (cart == null)
+      {
+        return BadRequest(new { msg = "Cart not found" });
+      }
+
+      cartItem = await _context.CartItems.Where(c => c.CartId == cart.Id && c.ProductId == changeQuantityParams.ProductId).FirstOrDefaultAsync();
+      if (cartItem == null)
+      {
+        return BadRequest(new { msg = "CartItem not found" });
+      }
+
+      if (product.Available < changeQuantityParams.Quantity)
+      {
+        return BadRequest(new { msg = $"Product only contains {product.Available} items less" });
+      }
+
+      cartItem.Quantity = changeQuantityParams.Quantity;
+      if (cartItem.Quantity <= 0)
+      {
+        _context.CartItems.Remove(cartItem);
+      }
+
+      await _context.SaveChangesAsync();
+
+      if (User.Identity.IsAuthenticated)
+      {
+        string userId = User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+        cart = await _context.Carts.Where(c => c.ApplicationUserId.Equals(userId)).Include(c => c.CartItems).FirstOrDefaultAsync();
+      }
+      else
+      {
+        cart = await _context.Carts.Where(c => c.SessionId.Equals(sessionId)).Include(c => c.CartItems).FirstOrDefaultAsync();
+
+      }
+      return Ok(cart);
+
     }
 
-    [HttpPost("DeleteFromCart/{productId}")]
-    public async Task<IActionResult> DeleteFromCart(string productId)
+    [HttpDelete("Cart/{productId}")]
+    public async Task<IActionResult> DeleteFromCart(int productId)
     {
+      var product = await _context.Products.FindAsync(productId);
+
+      if (product == null)
+      {
+        return NotFound(new { msg = "Product not found" });
+      }
+
       string sessionId = HttpContext.Request.Headers["SessionId"].FirstOrDefault();
+
+      if (string.IsNullOrEmpty(sessionId) && !User.Identity.IsAuthenticated)
+      {
+        return Unauthorized();
+      }
+
+      Cart cart = new Cart();
+      CartItem cartItem = new CartItem();
       if (User.Identity.IsAuthenticated)
       {
-        return Ok();
+        string userId = User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+        cart = await _context.Carts.Where(c => c.ApplicationUserId.Equals(userId)).FirstOrDefaultAsync();
+
+
 
       }
       else if (!string.IsNullOrEmpty(sessionId))
       {
-        return Ok();
+        cart = await _context.Carts.Where(c => c.SessionId.Equals(sessionId)).FirstOrDefaultAsync();
+
       }
+
+      if (cart == null)
+      {
+        return NotFound(new { msg = "Cart not found" });
+      }
+      cartItem = await _context.CartItems.Where(c => c.CartId == cart.Id && c.ProductId == productId).FirstOrDefaultAsync();
+
+      if (cartItem == null)
+      {
+        return NotFound(new { msg = "CartItem not found" });
+      }
+
+      _context.CartItems.Remove(cartItem);
+      await _context.SaveChangesAsync();
+      if (User.Identity.IsAuthenticated)
+      {
+        string userId = User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+        cart = await _context.Carts.Where(c => c.ApplicationUserId.Equals(userId)).Include(c => c.CartItems).FirstOrDefaultAsync();
+      }
+      else
+      {
+        cart = await _context.Carts.Where(c => c.SessionId.Equals(sessionId)).Include(c => c.CartItems).FirstOrDefaultAsync();
+
+      }
+      return Ok(cart);
+    }
+
+    [HttpGet("Cart")]
+    public async Task<IActionResult> GetCart()
+    {
+      string sessionId = HttpContext.Request.Headers["SessionId"].FirstOrDefault();
+
+      if (User.Identity.IsAuthenticated)
+      {
+        string userId = User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+
+        var cart = await _context.Carts.Where(c => c.ApplicationUserId.Equals(userId)).Include(c => c.CartItems).AsNoTracking().FirstOrDefaultAsync();
+        if (cart == null)
+        {
+          cart = new Cart();
+          cart.ApplicationUserId = userId;
+          _context.Carts.Add(cart);
+          await _context.SaveChangesAsync();
+        }
+
+        return Ok(cart);
+      }
+
+      else if (!string.IsNullOrEmpty(sessionId))
+      {
+        var cart = await _context.Carts.Where(c => c.SessionId.Equals(sessionId)).Include(c => c.CartItems).AsNoTracking().FirstOrDefaultAsync();
+        if (cart == null)
+        {
+          cart = new Cart();
+          cart.SessionId = sessionId;
+          _context.Carts.Add(cart);
+          await _context.SaveChangesAsync();
+        }
+
+        return Ok(cart);
+      }
+
       return Unauthorized();
     }
 
